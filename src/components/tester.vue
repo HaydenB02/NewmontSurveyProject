4e3969f019e6418df6293b1f446183c490358cdc
OchoumareD <dochoumare123@gmail.com>
2022-10-13 16:44:47 -0600

    fixed three js issue

--- src/components/tester.vue
+++ src/components/tester.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="tester"></div>
 </template>
-
+<script async src="https://unpkg.com/es-module-shims@1.3.6/dist/es-module-shims.js"></script>
 <script lang="ts">
 //Imports
 import Vue from "vue";
